9d85fbaf44a0bfb0a5e8c626851a5ebf16ced461
wyfffffei <h3wyf@126.com>
2022-01-12 17:53:31 +0800

    pdf & function update

---
+++ classification/func_gradient_loss.py
@@ -0,0 +1,19 @@
+def _cross_entropy_loss(y_pred, Y_label):
+    # This function computes the cross entropy.
+    #
+    # Arguements:
+    #     y_pred: probabilistic predictions, float vector
+    #     Y_label: ground truth labels, bool vector
+    # Output:
+    #     cross entropy, scalar
+    cross_entropy = -np.dot(Y_label, np.log(y_pred)) - np.dot((1 - Y_label), np.log(1 - y_pred))
+    return cross_entropy
+
+def _gradient(X, Y_label, w, b):
+    # This function computes the gradient of cross entropy loss with respect to weight w and bias b.
+    y_pred = _f(X, w, b)
+    pred_error = Y_label - y_pred
+    w_grad = -np.sum(pred_error * X.T, 1)
+    b_grad = -np.sum(pred_error)
+    return w_grad, b_grad
+
---
+++ classification/func_update_parameters.py
@@ -0,0 +1,32 @@
+def _shuffle(X, Y):
+    # This function shuffles two equal-length list/array, X and Y, together.
+    randomize = np.arange(len(X))
+    np.random.shuffle(randomize)
+    return (X[randomize], Y[randomize])
+
+def _sigmoid(z):
+    # Sigmoid function can be used to calculate probability.
+    # To avoid overflow, minimum/maximum output value is set.
+    return np.clip(1 / (1.0 + np.exp(-z)), 1e-8, 1 - (1e-8))
+
+def _f(X, w, b):
+    # This is the logistic regression function, parameterized by w and b
+    #
+    # Arguements:
+    #     X: input data, shape = [batch_size, data_dimension]
+    #     w: weight vector, shape = [data_dimension, ]
+    #     b: bias, scalar
+    # Output:
+    #     predicted probability of each row of X being positively labeled, shape = [batch_size, ]
+    return _sigmoid(np.matmul(X, w) + b)
+
+def _predict(X, w, b):
+    # This function returns a truth value prediction for each row of X 
+    # by rounding the result of logistic regression function.
+    return np.round(_f(X, w, b)).astype(np.int)
+    
+def _accuracy(Y_pred, Y_label):
+    # This function calculates prediction accuracy
+    acc = 1 - np.mean(np.abs(Y_pred - Y_label))
+    return acc
+
