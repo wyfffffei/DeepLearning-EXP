7a1f8a6bb283759737a65a278e8da6c45bd36be7
wyfffffei <h3wyf@126.com>
2022-02-23 18:04:27 +0800

    improve data augmentation

--- CNN/hw3_main.py
+++ CNN/hw3_main.py
@@ -13,8 +13,9 @@ import time
 def _readfile(path, label):
     # label 是一個 boolean variable，代表需不需要回傳 y 值
     image_dir = sorted(os.listdir(path))
-    x = np.zeros((len(image_dir), 128, 128, 3), dtype=np.uint8)
-    y = np.zeros(len(image_dir), dtype=np.uint8) if label else None
+    x, y = np.zeros((len(image_dir), 128, 128, 3), dtype=np.uint8), None
+    if label:
+        y = np.zeros(len(image_dir), dtype=np.uint8)
     for i, file in enumerate(image_dir):
         img = cv2.imread(os.path.join(path, file))
         x[i, :, :] = cv2.resize(img, (128, 128))
--- CNN/test.py
+++ CNN/test.py
@@ -1,20 +1,21 @@
 import pandas as pd
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
 from torchvision.transforms import transforms
-from CNN.main import _readfile
+from torchvision.utils import save_image
+
+from CNN.hw3_main import _readfile
 
 import numpy as np
 import model
 import os
 import torch
 
-# predict = pd.read_csv("./predict.csv")
 
+# # check in windows
 # for d in range(11):
 #     os.mkdir(f"./output/{d}")
-
-# check in windows
 # for pic in predict.values:
 #     file = str(pic[0]).zfill(4)
 #     target = str(pic[1])
@@ -24,37 +25,34 @@ import torch
 
 workspace_dir = './food-11'
 # print("Reading data ..")
-# train_x, train_y = _readfile(os.path.join(workspace_dir, "training"), True)
-# print("Size of training data = {}".format(len(train_y)))
-# test_x = _readfile(os.path.join(workspace_dir, "1"), False)
-# print("Size of Testing data = {}".format(len(test_x)), end="\n\n")
+test_x = _readfile(os.path.join(workspace_dir, "temp"), False)
 
-# train_transform = transforms.Compose([
-#     transforms.ToPILImage(),
-#     transforms.RandomHorizontalFlip(),  # 隨機將圖片水平翻轉
-#     transforms.RandomRotation(15),  # 隨機旋轉圖片
-#     transforms.ToTensor(),  # 將圖片轉成 Tensor，並把數值 normalize 到 [0,1] (data normalization)
-# ])
-#
-# batch_size = 1
-# x = torch.rand(8, 2, 2)
-# print(x)
-#
-# train_set = model.ImgDataset(x, transform=train_transform)
+test_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomHorizontalFlip(),  # 隨機將圖片水平翻轉
+    transforms.RandomRotation(15),  # 隨機旋轉圖片
+    transforms.ToTensor(),  # 將圖片轉成 Tensor，並把數值 normalize 到 [0,1] (data normalization)
+])
+
+# test_y = ImageFolder(workspace_dir, transform=train_transform)
+test_set = model.ImgDataset(test_x, transform=test_transform)
 # print("Size of training data = {}".format(train_set.__len__()))
-# # train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=False)
-#
-# for data in train_set:
-#     print(data)
+# train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=False)
+
+# data augmentation
+for epo in range(10):
+    for num, img in enumerate(test_set):
+        print(img.shape)
+        save_image(img, "./food-11/temp/img" + str(epo * test_set.__len__() + num) + ".png")
 
 
-# Note
-def test_1(x, y, flag):
-    if flag:
-        return x, y
-    else:
-        return y
-def test_2(x, y, flag):
-    return x, y if flag else x
-print(test_1(1, 2, False))
-print(test_2(1, 2, False))
+# #  Note
+# def test_1(x, y, flag):
+#     if flag:
+#         return x, y
+#     else:
+#         return y
+# def test_2(x, y, flag):
+#     return x, y if flag else x
+# print(test_1(1, 2, False))
+# print(test_2(1, 2, False))
